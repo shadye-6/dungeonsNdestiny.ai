dd76e7416e4d62f4a67362794a06130eb0748f81
shadye-6 <dxkshin66@gmail.com>
2025-10-11 20:31:05 +0530

    ui v4

--- utils/config.py
+++ utils/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MONGO_URI_PASSWORD = os.getenv("MONGODB_PASSWORD")
 
 MODEL_NAME = "gemini-2.5-flash-lite"
 MAX_TURNS_WORKING_MEMORY = 5  # short-term memory
@@ -13,7 +14,11 @@ MEMORY_INDEX_PATH = "storage/memory_index.pkl"
 
 LOG_LEVEL = "INFO"
 
-MONGO_URI = "mongodb+srv://shadye:<password>@textembeddings.lxxktdc.mongodb.net/?retryWrites=true&w=majority&appName=TextEmbeddings"
+MONGO_URI = (
+    f"mongodb+srv://shadye:{MONGO_URI_PASSWORD}"
+    "@textembeddings.lxxktdc.mongodb.net/"
+    "?retryWrites=true&w=majority&appName=TextEmbeddings"
+)
 MONGO_DB_NAME = "dungeon_master"
 MONGO_COLLECTION_NAME = "memories"
 
